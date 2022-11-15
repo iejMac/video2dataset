a2b6a1859dff84129e0bf6301581e392682b91c0
iejmac <iejmac@gpu-st-p4d-24xlarge-6.hpc-1click-sandbox.pcluster>
2022-11-15 19:09:54 +0000

    add more params

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 mkdir -p dataset
-video2dataset benchmark_vids.parquet --dest dataset
-
-
+video2dataset benchmark_vids.parquet --dest="dataset" --output-format="files" --metadata-columns="videoID,title,description,start,end"
