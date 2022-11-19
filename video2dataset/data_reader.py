158a5758965968d9c8233b00609555de79ee2b26
iejMac <kilianmaciej6@gmail.com>
2022-11-19 23:47:12 +0000

    update

--- video2dataset/data_reader.py
+++ video2dataset/data_reader.py
@@ -4,6 +4,37 @@ import tempfile
 import time
 import yt_dlp
 
+from multiprocessing import get_context
+
+
+def get_fast_format(formats, timeout):
+
+    def check_speed(f):
+        url = f.get('url')
+        ntf, _ = handle_mp4_link(url)
+        with open(ntf.name, "rb") as f:
+            vid_bytes = f.read()
+        ntf.close()
+        return
+
+    format_id = None
+    for f in formats:
+        ctx = get_context("spawn")
+        p = ctx.Process(target=check_speed, args=(f,))
+
+        p.start()
+        t0 = time.time()
+        while time.time() - t0 <= timeout:
+            if not p.is_alive():
+                format_id = f.get("format_id")
+                break
+            time.sleep(.1)
+        p.terminate()
+        p.join()
+
+        if format_id is not None:
+            break
+    return format_id
 
 
 def handle_youtube(youtube_url, retries, timeout, video_height, video_width):
@@ -11,6 +42,7 @@ def handle_youtube(youtube_url, retries, timeout, video_height, video_width):
     # Probe download speed:
     ydl_opts = {
         "quiet": True,
+        "socket_timeout": timeout,
         "external-download": "ffmpeg",
         "external-downloader-args": "ffmpeg_i:-ss 0 -t 2", # download 2 seconds
     }
@@ -19,24 +51,15 @@ def handle_youtube(youtube_url, retries, timeout, video_height, video_width):
     formats = info.get("formats", None)
     filtered_formats = [f for f in formats if f['height'] is not None and f['height'] >= video_height and f['width'] >= video_width]
 
-    format_id = None
-    # TODO: stop downloading after timeout (had a case where it took 46s)
-    for f in filtered_formats[:retries + 1]:
-        t0 = time.time()
-        ntf, _ = handle_mp4_link(f["url"])
-        with open(ntf.name, "rb") as vid_file:
-            vid_bytes = vid_file.read()
-        ntf.close()
-        tf = time.time()
-        print(f"{tf - t0} | {timeout}")
-        if tf - t0 < timeout:
-            format_id = f["format_id"]
-            break
+    print(len(filtered_formats)) 
+    # TODO: how do we drop the video when format_id is None (all retires timed out)
+    format_id = get_fast_format(filtered_formats[:retries+1], timeout)
 
     # Get actual video:
     # TODO: figure out a way of just requesting the format by format_id
     ydl_opts = {
         "quiet": True,
+        "socket-timeout": timeout
     }
     ydl = yt_dlp.YoutubeDL(ydl_opts)
     info = ydl.extract_info(youtube_url, download=False)
@@ -95,7 +118,9 @@ class VideoDataReader:
     def __call__(self, row):
         key, url = row
         file_name, file, _ = handle_url(url, self.retries, self.timeout, self.format_args)
+        t0 = time.time()
         with open(file_name, "rb") as vid_file:
             vid_bytes = vid_file.read()
+        print(f"video downloading took {time.time() - t0}")
         file.close()
         return key, vid_bytes, None
