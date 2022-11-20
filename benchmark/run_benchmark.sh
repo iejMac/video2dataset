d52a3aa18f10becd49a41ae4c691c3d0840ecc38
iejMac <kilianmaciej6@gmail.com>
2022-11-20 01:45:48 +0000

    update benchmark

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -3,10 +3,14 @@
 rm -rf dataset
 
 video2dataset --url_list="benchmark_vids.parquet" \
---input_format="parquet" \
---output_folder="dataset" \
---output-format="files" \
---url_col="videoLoc" \
---caption_col="title" \
---save_additional_columns='[videoID,description,start,end]' \
---enable_wandb=True
+        --input_format="parquet" \
+        --output_folder="dataset" \
+        --output-format="files" \
+        --url_col="videoLoc" \
+        --caption_col="title" \
+        --save_additional_columns='[videoID,description,start,end]' \
+        --enable_wandb=False \
+        --video_height=360 \
+        --video_width=640 \
+        --timeout 2 \
+        --retries 2 \
