accfe7bea0a67f69eb76df843a6f7f6d695f529d
iejMac <kilianmaciej6@gmail.com>
2022-11-23 05:39:28 +0000

    update wandb

--- examples/download_webvid.sh
+++ examples/download_webvid.sh
@@ -9,8 +9,8 @@ video2dataset --url_list="results_2M_val.csv" \
         --url_col="contentUrl" \
         --caption_col="name" \
         --save_additional_columns='[videoid,page_idx,page_dir,duration]' \
-        --enable_wandb=False \
+        --enable_wandb=True \
         --video_height=360 \
         --video_width=640 \
-        --number_sample_per_shard=10 \
+        --number_sample_per_shard=1000 \
         --processes_count 10 
