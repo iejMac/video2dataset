6b1e4ebc64ddf3a960780d62847243708ae266b1
iejMac <kilianmaciej6@gmail.com>
2022-11-23 10:36:35 +0000

    check output

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,33 @@
+import pandas as pd
 import pytest
+import tarfile
+
 from video2dataset.main import video2dataset
 
 
 def test_e2e():
     url_list = "tests/test_files/test_webvid.csv"
+    destination = "tests/dataset"
+
+    samples_per_shard = 10
+
     video2dataset(
         url_list,
+        output_folder=destination,
         input_format="csv",
         output_format="webdataset",
-        output_folder="tests/dataset",
         url_col="contentUrl",
         caption_col="name",
         save_additional_columns=["videoid","page_idx","page_dir","duration"],
         video_height=360,
         video_width=640,
-        number_sample_per_shard=10,
-        processes_count=2,
+        number_sample_per_shard=samples_per_shard,
+        processes_count=1,
     )
+
+    for shard in ["00000", "00001"]:
+        for ext in ["mp4", "json", "txt"]:
+            assert (
+                len([x for x in tarfile.open(destination + f"/{shard}.tar").getnames() if x.endswith(f".{ext}")])
+                == samples_per_shard
+            )
