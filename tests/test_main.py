2dc152983300ed469dc99c309d6a4a4ea6a30e39
iejMac <kilianmaciej6@gmail.com>
2022-11-26 07:56:38 +0000

    use tempfile

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,33 +1,33 @@
 import pandas as pd
 import pytest
 import tarfile
+import tempfile
 
 from video2dataset.main import video2dataset
 
 
 def test_e2e():
-    url_list = "tests/test_files/test_webvid.csv"
-    destination = "tests/dataset"
+    with tempfile.TemporaryDirectory() as tmpdir:
+        url_list = "tests/test_files/test_webvid.csv"
+        samples_per_shard = 10
 
-    samples_per_shard = 10
+        video2dataset(
+            url_list,
+            output_folder=tmpdir,
+            input_format="csv",
+            output_format="webdataset",
+            url_col="contentUrl",
+            caption_col="name",
+            save_additional_columns=["videoid","page_idx","page_dir","duration"],
+            video_height=360,
+            video_width=640,
+            number_sample_per_shard=samples_per_shard,
+            processes_count=1,
+        )
 
-    video2dataset(
-        url_list,
-        output_folder=destination,
-        input_format="csv",
-        output_format="webdataset",
-        url_col="contentUrl",
-        caption_col="name",
-        save_additional_columns=["videoid","page_idx","page_dir","duration"],
-        video_height=360,
-        video_width=640,
-        number_sample_per_shard=samples_per_shard,
-        processes_count=1,
-    )
-
-    for shard in ["00000", "00001"]:
-        for ext in ["mp4", "json", "txt"]:
-            assert (
-                len([x for x in tarfile.open(destination + f"/{shard}.tar").getnames() if x.endswith(f".{ext}")])
-                == samples_per_shard
-            )
+        for shard in ["00000", "00001"]:
+            for ext in ["mp4", "json", "txt"]:
+                assert (
+                    len([x for x in tarfile.open(tmpdir + f"/{shard}.tar").getnames() if x.endswith(f".{ext}")])
+                    == samples_per_shard
+                )
