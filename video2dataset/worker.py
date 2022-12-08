b0ec79defea0ee2e7f970de45de34b15c366a58f
iejmac <iejmac@ip-26-0-129-68.ec2.internal>
2022-12-08 02:00:54 +0000

    worker

--- video2dataset/worker.py
+++ video2dataset/worker.py
@@ -122,7 +122,7 @@ class Worker:
 
         with ThreadPool(self.thread_count) as thread_pool:
             for key, vid_stream, error_message in thread_pool.imap_unordered(
-                lambda x: self.data_loader(x),
+                lambda x: self.data_reader(x),
                 loader,
             ):
                 try:
