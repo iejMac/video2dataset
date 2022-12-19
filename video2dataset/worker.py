79b30b9a39a402031bb36b6c5d92cb8aa50a0c2d
Maciej Kilian <kilianmaciej6@gmail.com>
2022-12-19 13:04:01 +0100

    logging: remove video-specific youtube errors (#72)
    * logging: remove video-specific youtube errors
    
    * more specific
    
    * works
    
    * fix lint

--- video2dataset/worker.py
+++ video2dataset/worker.py
@@ -138,6 +138,8 @@ class Worker:
                     }
 
                     if error_message is not None:
+                        if "[youtube]" in error_message:  # video-specific error, remove videoID
+                            error_message = "ERROR: [youtube]:" + error_message.split(":")[-1]
                         failed_to_download += 1
                         status = "failed_to_download"
                         status_dict.increment(error_message)
