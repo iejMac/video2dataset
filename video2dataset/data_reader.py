b931d6621edd7bd111098d320de604c2c27d743d
iejmac <iejmac@ip-26-0-130-73.ec2.internal>
2022-12-23 12:27:06 +0000

    update

--- video2dataset/data_reader.py
+++ video2dataset/data_reader.py
@@ -107,12 +107,31 @@ def handle_youtube(youtube_url, tmp_dir, yt_metadata_args, video_height, video_w
     return path, yt_meta_dict, None
 
 
-def handle_mp4_link(mp4_link, tmp_dir, dl_timeout):
-    resp = requests.get(mp4_link, stream=True, timeout=dl_timeout)
-    path = f"{tmp_dir}/{str(uuid.uuid4())}.mp4"
-    with open(path, "wb") as f:
-        f.write(resp.content)
-    return path, None
+class YtDlpDownloader:
+    # TODO: maybe we just include height and width in the metadata_args
+    def __init__(self, tmp_dir, metadata_args, video_height, video_width):
+        self.tmp_dir = tmp_dir
+        self.metadata_args = metadata_args
+        self.video_height = video_height
+        self.video_width = video_width
+    def __call__(self, url):
+        path = f"{self.tmp_dir}/{str(uuid.uuid4())}.mp4"
+        format_string = (
+            f"bv*[height<={self.video_height}][width<={self.video_width}][ext=mp4]"
+            + f"+ba[ext=m4a]/b[height<={self.video_height}][width<={self.video_width}]"
+        )
+        ydl_opts = {
+            "outtmpl": path,
+            "format": format_string,
+            "quiet": True,
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(youtube_url)
+        if yt_metadata_args:
+            yt_meta_dict = get_yt_meta(youtube_url, self.metadata_args)
+        else:
+            yt_meta_dict = None, None
+        return path, yt_meta_dict, None
 
 
 class Mp4Downloader:
@@ -120,35 +139,18 @@ class Mp4Downloader:
         self.timeout = timeout
         self.tmp_dir = tmp_dir
     def __call__(self, url):
-        resp = requests.get(url, stream=True, timeout=dl_timeout)
-        path = f"{tmp_dir}/{str(uuid.uuid4())}.mp4"
+        resp = requests.get(url, stream=True, timeout=self.timeout)
+        path = f"{self.tmp_dir}/{str(uuid.uuid4())}.mp4"
         with open(path, "wb") as f:
             f.write(resp.content)
         return path, None
 
 
-class YtDlpDownloader:
-    def __init__(self):
-        pass
-    def __call__(self, url):
-        return url
-
-
 class VideoDataReader:
     """Video data reader provide data for a video"""
-
     def __init__(self, video_height, video_width, dl_timeout, tmp_dir, yt_meta_args) -> None:
-        self.format_args = {
-            "video_height": video_height,
-            "video_width": video_width,
-        }
-        self.dl_timeout = dl_timeout
-        self.tmp_dir = tmp_dir
-        self.yt_meta_args = yt_meta_args
-
-
         self.mp4_downloader = Mp4Downloader(dl_timeout, tmp_dir)
-        self.yt_downloader = YtDlpDownloader()
+        self.yt_downloader = YtDlpDownloader(tmp_dir, yt_meta_args, video_height, video_width)
 
     def __call__(self, row):
         key, url = row
@@ -162,9 +164,9 @@ class VideoDataReader:
                 file_path, yt_meta_dict, error_message = None, None, str(e)
         # TODO: add .avi, .webm, should also work
         elif url.endswith(".mp4"):  # mp4 link
-            file_path, error_message = handle_mp4_link(url, tmp_dir, dl_timeout)
+            file_path, error_message = self.mp4_downloader(url)
         else:
-            file_path, error_message = None, "Warning: Incorrect URL type"
+            file_path, error_message = None, "Warning: Unsupported URL type"
 
         if error_message is None:
             with open(file_path, "rb") as vid_file:
