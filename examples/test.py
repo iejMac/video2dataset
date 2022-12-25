62b3b319bcfef648c78a95f46f68eda8b1d6abb4
iejMac <kilianmaciej6@gmail.com>
2022-12-25 16:50:06 +0000

    move to examples

--- examples/test.py
+++ examples/test.py
@@ -1,6 +1,7 @@
+import time
 from data_loader import WebdatasetReader
 
-TARS = "pipe: aws s3 cp s3://s-laion/acav100m/mp4_acav100m/{00000..00010}.tar -"
+TARS = "pipe: aws s3 cp s3://s-laion/acav100m/mp4_acav100m/{00000..00000}.tar -"
 
 ds = WebdatasetReader(
     sampler=lambda a: a,
@@ -11,10 +12,14 @@ ds = WebdatasetReader(
     enable_metadata=True,
 )
 
+ct = 0
+
+t0 = time.time()
 for b in ds:
+    ct += 1
+    print(ct)
     print(b["video_tensor"].shape)
+tf = time.time()
 
-
-
-
-
+print("VID/S:")
+print(ct/(tf-t0))
