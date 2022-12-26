afc5096a779af3aa699d02e0123fef5bf437f221
iejMac <kilianmaciej6@gmail.com>
2022-12-26 16:19:23 +0000

    fix lint

--- video2dataset/subsamplers/noop_subsampler.py
+++ video2dataset/subsamplers/noop_subsampler.py
@@ -1,6 +1,9 @@
 """No operation subsampler"""
+
+
 class NoOpSubsampler:
     def __init__(self):
         pass
+
     def __call__(self, video_bytes, metadata):
         return [video_bytes], [metadata], None
