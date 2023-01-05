543318bfbcf611047f4d865f74ce076b1537a22b
iejMac <kilianmaciej6@gmail.com>
2023-01-05 15:53:58 +0000

    fix black

--- video2dataset/data_reader.py
+++ video2dataset/data_reader.py
@@ -112,10 +112,14 @@ class YtDlpDownloader:
 
         # Download the best video available with the mp4 extension but no better than 480p,
         # or the worst video with the mp4 extension if there is no video under 480p
-        format_string = f"bv*[height<={self.video_size}][ext=mp4]+ba/b[height<={self.video_size}][ext=mp4] / wv*+ba/w[ext=mp4]"
+        format_string = (
+            f"bv*[height<={self.video_size}][ext=mp4]+ba/b[height<={self.video_size}][ext=mp4] / wv*+ba/w[ext=mp4]"
+        )
         # Download the worst video available with the mp4 extension but no worse than 480p,
         # or the best video with the mp4 extension if there is no video over 480p
-        format_string = f"wv*[height>={self.video_size}][ext=mp4]+wa/w[height>={self.video_size}][ext=mp4] / bv*+ba/b[ext=mp4]"
+        format_string = (
+            f"wv*[height>={self.video_size}][ext=mp4]+wa/w[height>={self.video_size}][ext=mp4] / bv*+ba/b[ext=mp4]"
+        )
 
         ydl_opts = {
             "outtmpl": path,
