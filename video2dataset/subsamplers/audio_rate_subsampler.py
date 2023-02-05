cdc935e80d70065f8c3cc3644082329b85f213ee
iejMac <kilianmaciej6@gmail.com>
2023-02-05 13:06:25 +0000

    subsamplers: move audio rate subsampling to it's own subsampler

---
+++ video2dataset/subsamplers/audio_rate_subsampler.py
@@ -0,0 +1,30 @@
+"""
+frame subsampler adjusts the fps of the videos to some constant value
+"""
+
+
+import tempfile
+import os
+import ffmpeg
+
+
+class AudioRateSubsampler:
+    """
+    Adjusts the frame rate of the videos to the specified frame rate.
+    Args:
+        frame_rate (int): Target frame rate of the videos.
+    """
+    def __init__(self, sampling_rate):
+        self.sampling_rate = sampling_rate
+
+    def __call__(self, audio_bytes):
+        subsampled_bytes = []
+        for vid_bytes in video_bytes:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                try:
+                    # some ffmpeg func
+                except Exception as err:  # pylint: disable=broad-except
+                    return [], str(err)
+
+                # append some stuff to subsampled_bytes
+        return subsampled_bytes, None
