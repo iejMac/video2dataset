8765e3740c80cd0994042838ffa07e09e3269ef3
iejMac <kilianmaciej6@gmail.com>
2023-03-12 07:36:41 +0000

    benchmark

--- benchmark/benchmark_dataloader.py
+++ benchmark/benchmark_dataloader.py
@@ -7,8 +7,9 @@ from video2dataset.dataloader import get_bytes_dataloader
 # Benchmark videos are the WebVid validation split (5000 videos)
 SHARDS = "examples/dataset/{00000..00004}.tar"
 
-def benchmark_bytes_dl(workers):
-    dl = get_bytes_dataloader(SHARDS, 48)
+def benchmark_bytes_dl():
+    # 326.06278216697706
+    dl = get_bytes_dataloader(SHARDS, 12)
 
     count = 0
     t0 = time.time()
@@ -17,7 +18,8 @@ def benchmark_bytes_dl(workers):
         count += 1
     tf = time.time()
     return count/(tf-t0)
-    print(count/(tf-t0))
 
 
-
+if __name__ == "__main__":
+    print("Benchmarking bytes dataloader...")
+    print(benchmark_bytes_dl())
