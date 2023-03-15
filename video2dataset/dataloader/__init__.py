913a86c557b7947f70a30aadb8c3e0694fb906a6
ablattmann <andreas.blattmann@iwr.uni-heidelberg.de>
2023-03-15 10:11:35 +0100

    add webdataset for training models

--- video2dataset/dataloader/__init__.py
+++ video2dataset/dataloader/__init__.py
@@ -4,3 +4,4 @@ Load the data (videos and metadata) into bytes, tensors, strings etc.
 
 from .bytes_dataloader import get_bytes_dataloader
 from .train_dataloader import get_video_data  # WIP, currently works for open_clip
+from .train_dataloader import get_video_dataset
