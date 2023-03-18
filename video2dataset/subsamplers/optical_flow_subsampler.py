586176a8d33ba011f78d3264dcf3b4ebc75782fd
danielmend <daniel.mendelevitch@gmail.com>
2023-03-18 05:00:15 +0000

    parameterize cv2 subsampler

--- video2dataset/subsamplers/optical_flow_subsampler.py
+++ video2dataset/subsamplers/optical_flow_subsampler.py
@@ -11,13 +11,31 @@ from datetime import datetime
 import torch
 import numpy as np
 
+class Cv2Detector:
+    def __init__(self, pyr_scale=0.5, levels=3, winsize=15, iterations=3, poly_n=5, poly_sigma=1.2, flags=0):
+        self.pyr_scale = pyr_scale
+        self.levels = levels
+        self.winsize = winsize
+        self.iterations = iterations
+        self.poly_n = poly_n
+        self.poly_sigma = poly_sigma
+        self.flags = flags
+
+    def __call__(self, frame1, frame2):
+        return cv2.calcOpticalFlowFarneback(frame1, frame2, None, self.pyr_scale, self.levels, self.winsize, self.iterations, self.poly_n, self.poly_sigma, self.flags)
+        
 class OpticalFlowSubsampler:
     """
     Detects optical flow in video frames
     """
 
-    def __init__(self, detector="cv2", fps=-1):
-        self.detector = detector
+    def __init__(self, detector="cv2", fps=-1, cv2_params=None, raft_params=None):
+        if detector == "cv2":
+            if cv2_params:
+                pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags = cv2_params
+                self.detector = Cv2Detector(pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags)
+            else:
+                self.detector = Cv2Detector()
         self.fps = fps
 
     def __call__(self, video_bytes):
@@ -51,9 +69,9 @@ class OpticalFlowSubsampler:
 
                     if fc % take_every_nth != 0:
                         continue
-                    
+
                     next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-                    flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+                    flow = self.detector(prvs, next)
 
                     optical_flow.append(flow)
                     prvs = next
