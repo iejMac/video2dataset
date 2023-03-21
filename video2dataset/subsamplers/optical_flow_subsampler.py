d5449e14883c6b74a6f52466f234fca56ef0ddf0
danielmend <daniel.mendelevitch@gmail.com>
2023-03-21 23:19:08 +0000

    take out RAFT (for now)

--- video2dataset/subsamplers/optical_flow_subsampler.py
+++ video2dataset/subsamplers/optical_flow_subsampler.py
@@ -10,55 +10,6 @@ import torch
 from raft.raft import RAFT
 from raft.utils import InputPadder
 
-class AttrDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-class RAFTDetector:
-    def __init__(self, model_name="raft-things", small=False, mixed_precision=False, alternate_corr=False, device='cuda'):
-        self.args = AttrDict(dict(
-            model=model_name,
-            small=small,
-            mixed_precision=mixed_precision,
-            alternate_corr=alternate_corr
-        ))
-
-        self.model = RAFT(self.args)
-
-        ## DOWNLOAD WEIGHTS HERE ##
-        weights_path = None # do something with model_name
-        ## ===================== ##
-
-        self.model.load_state_dict(
-            torch.load(weights_path)
-        )
-
-        model = model.module
-
-        self.device = device
-        model.to(device)
-        model.eval()
-
-        self.padder = None
-
-    def preprocess(self, frame):
-        if not self.padder:
-            self.padder = InputPadder(frame.shape)
-            
-        tens = torch.from_numpy(frame).permute(2, 0, 1).float()
-        return tens[None].to(self.device)
-
-    def __call__(self, f1, f2):
-        f1, f2 = self.preprocess(f1), self.preprocess(f2)
-        f1, f2 = self.padder.pad(f1, f2)
-
-        _, flow_up = self.model(f1, f2, iters=20, test_mode=True)
-        return flow_up
-        
-    def reset_padder(self):
-        self.padder = None
-
 class Cv2Detector:
     """
     A class to perform optical flow detection using OpenCV's Farneback method.
@@ -127,16 +78,6 @@ class OpticalFlowSubsampler:
                 self.detector = Cv2Detector(pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags)
             else:
                 self.detector = Cv2Detector()
-        elif detector == "RAFT":
-            if params:
-                model_name, is_small, mixed_precision, alternate_corr = params
-                if not device:
-                    device = "cuda:0" # TODO: can i do this better?
-                self.detector = RAFTDetector(model_name, is_small, mixed_precision, alternate_corr, device)
-            else:
-                if not device:
-                    device = "cuda:0"
-                self.detector = RAFTDetector(device=device)
         else:
             raise NotImplementedError()
 
