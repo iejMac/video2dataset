416656e2eb657b4f1ab7aef8195e5e20d612da12
Maciej Kilian <kilianmaciej6@gmail.com>
2023-03-21 17:34:57 -0700

    More robust and flexible dataloader (bytes_dataloader deprecated) (#124)
    * more robust train dataloader (#123)
    
    * avoid issues with excessive quotes in reading caption in csv files
    
    * add webdataset for training models
    
    ---------
    
    Co-authored-by: ablattmann <andreas@stability.ai>
    Co-authored-by: ablattmann <andreas.blattmann@iwr.uni-heidelberg.de>
    
    * get_video_dataset: remove args, add params + docstring (#126)
    
    * get_video_dataset: remove args, add params + docstring
    
    * small changes
    
    * add TODO
    
    * fix del sample[self.video_key]
    
    * fix black
    
    * pylint many fixes
    
    * fix name
    
    * remove yaml, don't imagine when we'd need + causes problems with lint
    
    * fix black
    
    * remove open_clip stuff
    
    * remove open_clip
    
    * one shuffle param
    
    * remove open_clip transforms
    
    * update
    
    * fix black
    
    * fix pylint
    
    * update subset worker
    
    ---------
    
    Co-authored-by: ablattmann <andreas@stability.ai>
    Co-authored-by: ablattmann <andreas.blattmann@iwr.uni-heidelberg.de>

--- video2dataset/dataloader/__init__.py
+++ video2dataset/dataloader/__init__.py
@@ -2,5 +2,4 @@
 Load the data (videos and metadata) into bytes, tensors, strings etc.
 """
 
-from .bytes_dataloader import get_bytes_dataloader
-from .train_dataloader import get_video_data  # WIP, currently works for open_clip
+from .dataloader import get_video_dataset
