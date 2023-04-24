d59644f2414a0fee9533d150513596c54cc32e4c
Marianna <mariana13019940@gmail.com>
2023-04-24 20:54:42 +0000

    fixed backend

--- video2dataset/dataloader/audio_decode.py
+++ video2dataset/dataloader/audio_decode.py
@@ -1,10 +1,10 @@
-"""Audio Decoders"""
+"""Audio Decoder"""
 import torchaudio
 import io
 
 
 def set_backend(extension):
-    """Set torchaudio backend for different extensions (soundfile doesn't support M4A and MP3)"""
+    """Sets torchaudio backend for different extensions (soundfile doesn't support M4A and MP3)"""
     if extension in ["wav", "flac"]:
         torchaudio.set_audio_backend("soundfile")
     else:
@@ -14,14 +14,13 @@ def set_backend(extension):
 class AudioDecoder:
     """Basic audio decoder that converts audio into torch tensors"""
 
-    def __init__(self, sample_rate=None, num_channels=None, extension='mp3'):
+    def __init__(self, sample_rate=None, num_channels=None, extension="wav"):
         self.sample_rate = sample_rate
         self.num_channels = num_channels
         set_backend(extension)
 
     def __call__(self, key, data):
         extension = key.split(".")[-1]
-
         if extension not in "mp3 wav flac m4a".split():
             return None
         additional_info = {}
