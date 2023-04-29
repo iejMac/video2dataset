d57c88734275f6d3842be527ff50cc9685294e2d
iejmac <iejmac@ip-26-0-144-144.us-west-2.compute.internal>
2023-04-29 06:52:35 +0000

    update

--- benchmark/download_set.sh
+++ benchmark/download_set.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-rm -rf dataset
-
-video2dataset --url_list="video_sets/youtube.parquet" \
+video2dataset --url_list="video_sets/mp4.parquet" \
         --input_format="parquet" \
-        --output_folder="dataset" \
+        --output_folder="dataset/mp4" \
         --output-format="webdataset" \
-        --url_col="videoLoc" \
-        --caption_col="title" \
-        --enable_wandb=True \
+        --url_col="contentUrl" \
+        --caption_col="name" \
+        --enable_wandb=False \
         --video_size=360 \
         --number_sample_per_shard=10 \
         --processes_count 10  \
-	--enable_wandb False \
