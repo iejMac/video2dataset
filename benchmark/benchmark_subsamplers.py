b7c9ac17df997a788d5179007aeb1951ad383722
Maciej Kilian <kilianmaciej6@gmail.com>
2023-05-09 11:07:44 +0200

    benchmarks: add config param (#184)
    * benchmarks: add config param
    
    * remove debug

--- benchmark/benchmark_subsamplers.py
+++ benchmark/benchmark_subsamplers.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import cv2
 import time
 import tempfile
@@ -53,12 +54,12 @@ def make_fake_clips(time, n):
     return [(i * clip_duration, (i + 1) * clip_duration) for i in range(n)]
 
 
-def main():
+def main(config_file="subsamplers_config.yaml"):
     # Gather system information
     system_info = gather_system_info()
 
     # Load config
-    benchmark_config = load_config('subsamplers_config.yaml')
+    benchmark_config = load_config(config_file)
 
     # Dataloader
     ds = get_video_dataset(
@@ -141,4 +142,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    elif len(sys.argv) == 1:
+        main()
+    else:
+        print("Usage: python yourscript.py [config_file]")
