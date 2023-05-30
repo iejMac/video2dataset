80b0b3ef2301a97968b5b26b1129a0a30367dbbb
iejMac <kilianmaciej6@gmail.com>
2023-05-30 13:47:46 +0000

    abcv

--- video2dataset/subsamplers/subsampler.py
+++ video2dataset/subsamplers/subsampler.py
@@ -1,11 +1,9 @@
 """Base subsampler class"""
+from abc import abstractmethod
 
 
 class Subsampler:
     """Subsamples input and returns in same format (stream dict + metadata)"""
-
-    def __init__(self):
-        raise NotImplementedError
-
+    @abstractmethod
     def __call__(self, streams, metadata):
-        raise NotImplementedError
+        raise NotImplementedError("Subsampler should not be called")
