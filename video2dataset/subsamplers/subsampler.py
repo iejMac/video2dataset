9fdd1ad113b7e918ad9eca9cd5554b50dde233c0
iejMac <kilianmaciej6@gmail.com>
2023-05-30 13:37:57 +0000

    fix black

--- video2dataset/subsamplers/subsampler.py
+++ video2dataset/subsamplers/subsampler.py
@@ -1,8 +1,11 @@
 """Base subsampler class"""
 
+
 class Subsampler:
     """Subsamples input and returns in same format (stream dict + metadata)"""
+
     def __init__(self):
         raise NotImplementedError
+
     def __call__(self, streams, metadata):
         raise NotImplementedError
