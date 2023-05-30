aadbb079b8c442c9f97847d9be09ae20b45f14dc
iejMac <kilianmaciej6@gmail.com>
2023-05-30 13:34:40 +0000

    Subsamplers: Add parent class

---
+++ video2dataset/subsamplers/subsampler.py
@@ -0,0 +1,8 @@
+"""Base subsampler class"""
+
+class Subsampler:
+    """Subsamples input and returns in same format (stream dict + metadata)"""
+    def __init__(self):
+        raise NotImplementedError
+    def __call__(self, streams, metadata):
+        raise NotImplementedError
