fd951e9c9343845ec9f91e69d2cfef2a2998b547
iejMac <kilianmaciej6@gmail.com>
2023-05-30 13:53:14 +0000

    fix black

--- video2dataset/subsamplers/subsampler.py
+++ video2dataset/subsamplers/subsampler.py
@@ -4,6 +4,7 @@ from abc import abstractmethod
 
 class Subsampler:
     """Subsamples input and returns in same format (stream dict + metadata)"""
+
     @abstractmethod
     def __call__(self, streams, metadata):
         raise NotImplementedError("Subsampler should not be called")
